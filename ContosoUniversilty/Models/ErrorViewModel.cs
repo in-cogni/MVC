cb99aa3e5d2e906d40f975e0bde3a157e78a65e5
LadaKashubo <lkashubo@yandex.ru>
2025-09-27 20:18:07 +0300

    Add project files.

---
+++ ContosoUniversilty/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContosoUniversilty.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
